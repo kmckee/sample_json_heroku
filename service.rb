f4ebf07da18226fe8cecd381508e3f879e25f9d6
Kyle McKee <mckee.kyle@gmail.com>
2015-02-16 14:55:11 -0500

    Added save that randomly fails (hopefully)

--- service.rb
+++ service.rb
@@ -3,6 +3,15 @@ require 'sinatra/json'
 
 # define a route that uses the helper
 get '/cities' do
-  response.headers["Access-Control-Allow-Origin"] = "*"
+  response.headers['Access-Control-Allow-Origin'] = '*'
   json :all => ['Cleveland', 'Warrensville', 'Solon', 'Avon Lake']
+end
+
+post '/save' do |data|
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  if [true, false].sample
+    raise NoMemoryError, 'OMG'
+  else
+    'OK'
+  end
 end
