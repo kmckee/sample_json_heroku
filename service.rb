e07bfe2ad7fcf24aea668d27ce62fa0cdfaa0aa4
Kyle McKee <mckee.kyle@gmail.com>
2015-02-16 14:10:46 -0500

    Allow cross site ajax.

--- service.rb
+++ service.rb
@@ -3,5 +3,6 @@ require 'sinatra/json'
 
 # define a route that uses the helper
 get '/cities' do
+  response.headers["Access-Control-Allow-Origin"] = "*"
   json :all => ['Cleveland', 'Warrensville', 'Solon', 'Avon Lake']
 end
