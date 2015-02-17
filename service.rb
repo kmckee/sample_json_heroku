614415f760f5db7330011a7913e5c4e9e52a2252
Kyle McKee <mckee.kyle@gmail.com>
2015-02-17 12:41:56 -0500

    Added details service.

--- service.rb
+++ service.rb
@@ -1,17 +1,25 @@
 require 'sinatra'
 require 'sinatra/json'
 
-# define a route that uses the helper
-get '/cities' do
+get '/nearby_cities/:id' do
   response.headers['Access-Control-Allow-Origin'] = '*'
+  raise ArgumentError unless params[:id] == '42'
   json :all => ['Cleveland', 'Warrensville', 'Solon', 'Avon Lake']
 end
 
 post '/save' do |data|
   response.headers['Access-Control-Allow-Origin'] = '*'
-  if [true, false].sample
-    raise NoMemoryError, 'OMG'
-  else
-    'OK'
-  end
+  'OK'
 end
+
+get '/details/:id' do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  raise ArgumentError unless params[:id] == '42'
+  json ({ cityName: "Ohio City",
+    county: "Cuyahoga",
+    population: "9,200",
+    medianIncome: "$20,000",
+    founded: "1836"
+      })
+end
+
